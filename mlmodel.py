2badc16ab31d20bc8ee16ae821d1d3ebced44b3e
Na Wang <na.wang-8@student.manchester.ac.uk>
2024-04-13 20:07:50 +0100

    Change reward func

--- mlmodel.py
+++ mlmodel.py
@@ -8,21 +8,29 @@ def rewardFunc (planets: list, original_num: int) -> float:
     # Position and Velocity are 3D vectors: [x,y,z]
     w1,w2 = 1,1
     reward = 0
-    reward += w1 * RNumPlanets(planets, original_num)  #[0,1]
-    reward += w2 * RConserved(planets)  # Reward for conserving energy
-    # NORMALISE REWARD HERE
-    reward = reward #HEREEEEEEEEEE
+    reward += w1 * RNumPlanets(planets, original_num)  #[0,1] - 0 is best, 1 is worst
+    reward += w2 * RConserved(planets)  #[0,1] - 0 is best, 1 is worst
+    #At this point reward is in the range [0,2] where 0 is best and 2 is worst
+
+    # Normalise reward to [-1,1] so that -1 is worst and 1 is best 
+    max_reward = w1 + w2
+    reward = -1*(reward-1)/max_reward
     return reward
 
 def RNumPlanets(planets: list, original_num: int) -> float:
     return (original_num-len(planets))/original_num
 
 def RConserved(objects):
+    # The smaller the sum the better (conservation of energy) - Centripetal and Gravitational Forces are in Equilibrium
+    # Returns a score of 1 if the sum is the best (energy all conserved), 0 if the sum is the worst (energy lost none conserved)
     sum = 0
     for object in objects:
         for object2 in objects:
             if object == object2:
                 continue
             sum += abs((phys.GRAVITATIONAL_CONSTANT * object2.mass)/np.linalg.norm(object2.position - object.position) - (object2.velocity)**2)
-    return sum
 
+    if sum == 0:
+        return 1
+    else:
+        return 1/sum
