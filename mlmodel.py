e8eceab38a3f55c9d0d646d07b8392a1f75ea42d
Na Wang <na.wang-8@student.manchester.ac.uk>
2024-04-13 18:07:40 +0100

    Fitness Function

--- mlmodel.py
+++ mlmodel.py
@@ -1,26 +1,35 @@
 import numpy as np
 import tensorflow as tf
 import pandas as pd
+import physics.py as phys
 
-def rewardFunc (object):
+NUMBEROFPLANETS = ...
+
+def rewardFunc (objectsOriginal):
     # Objects is a list of objects. Each object has properties: mass, radius, position, velocity
     # Position and Velocity are 3D vectors: [x,y,z]
-    w1,w2,w3,w4 = 0,0,0,1
+    NUMBEROFPLANETS = len(objectsOriginal)
+    objects = objectsOriginal
+    w1,w2 = 1,1
     reward = 0
-    reward += w1 * RBounded(object)  # Reward for staying within the bounds of the environment
-    reward += w2 * REscapeTraj(object)  # Reward for not having the trajectory escape the environment MIGHT LEAVE OUT
-    reward += w3 * RCollision(object)
-    reward += w4 * REnergy(object)  # Reward for conserving energy
+    for i in range(300):
+        objects = phys.updateAllObjects(objects)
+
+    reward += w1 * RNumPlanets(objects)  #[0,1]
+    reward += w2 * RConserved(objects)  # Reward for conserving energy
+
+    # NORMALISE REWARD HERE
+    reward = reward #HEREEEEEEEEEE
+
     return reward
 
-def RBounded(obect):
-    return 0
+def RNumPlanets(objects):
+    return (NUMBEROFPLANETS-len(objects))/NUMBEROFPLANETS
 
-def REscapeTraj(object):
-    return 0
-
-def RCollision(object):
-    return 0
-
-def REnergy(object):
-    return 0
+def RConserved(objects):
+    for object in objects:
+        for object2 in objects:
+            if object == object2:
+                continue
+            if not phys.calcGravitationalForce(object.mass, object2.mass, object.radius, object2.radius, object.position, object2.position) == 0:
+                return 0
