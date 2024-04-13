0baacf799c211f6f3728e6a60b0444e51925f79f
Samar Yadav <xtricity@Samars-MacBook-Pro.local>
2024-04-13 20:59:36 +0100

    lihwd

--- graphics.py
+++ graphics.py
@@ -1,20 +1,37 @@
+from direct.gui.DirectButton import DirectButton
 from direct.gui.DirectSlider import DirectSlider
-from panda3d.core import NodePath
 from direct.gui.DirectLabel import DirectLabel
 
 class Graphics:
     def __init__(self, base):
         self.aspect2d = base.aspect2d  # Get the aspect2d node from the base
-        self.aspect2d.setPos(0, 0, 0)  # Set the position of aspect2d node
         self.aspect2d.setDepthTest(False)
+                
         
-        self.slider = DirectSlider(range=(0, 100), command=self.slider_callback)
-        self.slider.setScale(0.5)
-        self.slider.setPos(-0.7, 0, 0.7)
-        self.slider.reparentTo(self.aspect2d)
+        self.button = DirectButton(text="Menu", command=self.show_slider_and_label)
+        self.button.setScale(0.1)
+        self.button.setPos(-0.9, 0, 0.8)
+        self.button.reparentTo(self.aspect2d)
 
-        self.label = DirectLabel(text="Slider Value: 0", pos=(-0.7, 0, 0.9), scale=0.1, parent=self.aspect2d)
+        self.slider = DirectSlider(range=(0, 100), command=self.slider_callback)
+        self.slider.setScale(0.3)
+        self.slider.setPos(-0.8, 0, 0.8)
+        self.slider.reparentTo(self.aspect2d)
+        self.slider.hide()
+
+        self.label = DirectLabel(text="Select number of planets: 0", pos=(-0.8, 0, 0.9), scale=0.06, parent=self.aspect2d)
+        self.label.setTransparency(1)
+        self.label.hide()
+
+    def show_slider_and_label(self):
+        if self.slider.isHidden():
+            self.slider.show()
+            self.label.show()
+            self.button.hide()
+        else:
+            self.slider.hide()
+            self.label.hide()
 
     def slider_callback(self):
         slider_value = round(self.slider['value'])
-        self.label['text'] = f"Slider Value: {slider_value}"
+        self.label['text'] = f"Number of planets: {slider_value}"
